9c37d4dce4ad1dee4321c14ae323ff709b2adcd6
Dan Matthew <111794+danielmatthew@users.noreply.github.com>
2021-05-07 11:22:08 +0100

    docs: update forms example

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./scss');
   eleventyConfig.addPassthroughCopy("./src/css");
-
   return {
+    pathPrefix: "/bootstrap-theme/",
     dir: {
       input: "src/docs",
       output: "docs",
